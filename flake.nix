18c80364e4941ed03d6a4db29f0e916e5e99bf87
arexon <arexonreal@gmail.com>
2024-08-23 13:42:29 +0300

    chore: update to v1.4.0

--- flake.nix
+++ flake.nix
@@ -19,13 +19,13 @@
 
       regolith = pkgs.buildGoModule rec {
         pname = "regolith";
-        version = "1.3.0";
+        version = "1.4.0";
 
         src = pkgs.fetchFromGitHub {
           owner = "Bedrock-OSS";
           repo = pname;
           rev = version;
-          sha256 = "sha256-H87cBGGG29wQ+SINham6FlUz+Udes7JtMjiSOy8J4L0=";
+          sha256 = "sha256-HZOkUyLBnBk3KOizpADrl6fBLY8mA10AI37rJDloaOs";
         };
 
         vendorHash = "sha256-+rr1sueoWER8IuI2bkv/vmIdfXZOydq4RjSHWBLwKsQ=";
