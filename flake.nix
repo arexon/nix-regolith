cb08a714a53adc8e9847664623dd5699af8e527c
arexon <arexonreal@gmail.com>
2024-12-04 16:18:08 +0200

    chore: update to v1.5.0

--- flake.nix
+++ flake.nix
@@ -17,22 +17,24 @@
 
       regolith = pkgs.buildGoModule rec {
         pname = "regolith";
-        version = "1.4.1";
+        version = "1.5.0";
 
         src = pkgs.fetchFromGitHub {
           owner = "Bedrock-OSS";
           repo = pname;
-          rev = version;
-          sha256 = "sha256-1nXyOojfXfcVoTUSwI96C0lJQBJZ9Zo4Ug1gUA3zd3Q=";
+          rev = "main";
+          sha256 = "sha256-DCvnDBtLTx3E1HnfbBcS0uEKRd/t92Rqro5J9T95JPY=";
         };
 
-        vendorHash = "sha256-+rr1sueoWER8IuI2bkv/vmIdfXZOydq4RjSHWBLwKsQ=";
+        vendorHash = "sha256-+4J4Z7lhbAphi6WUEJN9pzNXf6ROUKqN4NdKI2sQSW0=";
 
-        patchPhase = ''
-          sed -i 's/unversioned/${version}/g' main.go
-        '';
-
-        checkPhase = null;
+        doCheck = false;
+        ldflags = [
+          "-X main.buildSource=nix"
+          "-X main.version=${version}"
+          "-X main.commit=abd33df9f6022c4ba231bfed50b55f7559ef4612"
+          "-X main.date=19700101-00:00:00"
+        ];
       };
     in {
       formatter = pkgs.alejandra;
